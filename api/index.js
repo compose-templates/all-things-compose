707f25406e88c42bc2c8d91ebe1932e7c04c85bb
@k33g <ph.charriere@gmail.com>
2024-02-13 17:34:46 +0100

    🛟 Updated.

---
+++ api/index.js
@@ -0,0 +1,43 @@
+const fastify = require('fastify')({logger: true})
+
+const path = require('node:path')
+
+fastify.register(require('@fastify/redis'), { 
+    host: 'redis-server', 
+    port: 6379
+})
+
+
+const { ADDRESS = '0.0.0.0', PORT = '8080' } = process.env;
+
+fastify.get('/counter/:animal', (request, reply) => {
+    const { animal } = request.params;
+
+    const { redis } = fastify
+
+    redis.get(animal, (err, counter_value) => {
+        var counter = err ? 0 : Number(counter_value)
+        reply.send(counter)
+    })   
+})
+
+fastify.post('/counter', (request, reply) => {
+    const { redis } = fastify
+    let animal = request.body.animal
+
+    redis.get(animal, (err, counter_value) => {
+        var counter = err ? 0 : Number(counter_value)
+
+        redis.set(animal, counter+1, (err) => {
+            reply.send(err || { status: 'ok' })
+        })
+    })   
+})
+
+fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
